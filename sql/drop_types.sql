a956b243fbcfe2cf4b75f1cd214d5dc3fca85925
Rómulo Rodríguez <rodriguezrjrr@gmail.com>
2015-05-17 12:10:53 -0430

    Revert "Corrección de Estructura en Create.sql Funciones "table_""
    This reverts commit 9a16962b52252b8cad2afb32faeb55d13cd7c339.

--- sql/drop_types.sql
+++ sql/drop_types.sql
@@ -1,7 +1,13 @@
+
+DROP TYPE suelo;
 DROP TYPE exportacion;
-DROP TYPE receta;
+DROP TYPE tipolegislacion ;
 DROP TYPE ingrediente;
+DROP TYPE receta;
+DROP TYPE medida;
 DROP TYPE des_gen_region;
+DROP TYPE tipotelefono;
+DROP TYPE telefono;
 DROP TYPE res_solicitud;
 DROP TYPE direcciongeneral;
 DROP TYPE nombrepersona;
@@ -9,21 +15,5 @@ DROP TYPE cultivo;
 DROP TYPE historiabodega;
 DROP TYPE calificacion_vino;
 DROP TYPE costo;
-DROP TYPE fase_elaboracion_vino;
 DROP TYPE fas_ela_vin;
-DROP TYPE suelo;
-DROP TYPE legislacion;
-DROP TYPE tipolegislacion;
-DROP TYPE medida;
-DROP TYPE telefono;
-DROP TYPE tipotelefono;
-DROP TYPE premio;
-DROP TYPE pro_reco;
-DROP TYPE Valoracion;
-DROP TYPE val_clasificacion;
-DROP TYPE val_elemento;
-DROP TYPE val_fase;
-
-
-
-
+DROP TYPE fase_elaboracion_vino;

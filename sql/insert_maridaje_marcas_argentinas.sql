be9acebadca623ae42477cb46d9c66ed02472e88
gbsojo <gbsojo@gmail.com>
2015-05-28 02:21:55 -0400

    Listas las marcas italianas

--- sql/insert_maridaje_marcas_argentinas.sql
+++ sql/insert_maridaje_marcas_argentinas.sql
@@ -47,3 +47,149 @@ insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
 insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
 	(4,6,'Buena opción para acompañar quesos duros');
 
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(12,23,'Ideal para comidas ligeras acompañadas de vegetales');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(1,23,'Excelente opción para pescados');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(4,23,'Buena opción para quesos semi-maduros y maduros');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,23,'Armoniza perfectamente con carnes blancas jugosas');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,42,'Buena opción para carnes rojas grilladas');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,20,'Excelente opción para carnes rojas y  de caza como venado');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,20,'Marida idealmente con aver de corral');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(1,22,'Sus notas fuertes son ideales para pescados de alto contenido graso');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,22,'Buena opciòn para carne de aves como pollo o pavo');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(12,47,'Sus notas de frutos secos son ideales para acompañar vegetales grillados');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(13,47,'Armoniza perfectamente con risottos y arroces de sabores imponentes');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,40,'Ideal para carnes rojas. Especialmente cordero y cerdo');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(4,40,'Buena opción para platillos cargados de queso');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(2,39,'Sus notas frutales son ideales para acompañar mariscos grillados');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(8,39,'Buena opción para pastas de salsas ligeras');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(1,39,'Ideal para pescados jugosos, no grasosos');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(12,28,'Ideal para acompañar vegetales grillados');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(13,28,'Armoniza perfectamente con risottos y arroces de sabores imponentes');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(14,46,'Sus notas ligeras son ideales para una ensalada cesar o griega');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(1,46,'Buena opción para platillos de pescado ligeros');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,46,'Excelente opción para pollo grillado o a la parrilla');		
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,45,'Excelente opción para platillos grasosos como pato asado');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(10,19,'Armoniza especialmente con postres, ideal para el chocolate');		
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(4,19,'Combinar con quesos suaves y frescos');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(12,26,'Ideal para acompañar vegetales gratinados con quesos maduros');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(13,26,'Excelente opción para acompañar risottos cremosos y de sabores imponentes');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(12,21,'Combina muy bien con vegetales salteados con sabores agridulces');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(8,21,'Ideal para pastas con salsas de base de tomate');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,21,'Armoniza muy bien con aves de corral. Especialmente con alitas de pollo marinadas');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,21,'Sus notas de frutos salvajes combinan muy bien con carnes rojas y cordero');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,44,'Ideal para acompañar pollo');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,44,'Armoniza bien con carnes rojas ligeras');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(8,25,'Buena opción para pastas ligeras de sabores sutiles');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(4,25,'Ideal para acompañar quesos maduros');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(14,25,'Maridar con ensaladas mediterraneas');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(15,48,'Ideal para acompañar embutidos, como salchichas de cerdo');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,48,'Buen acompañante para carne de cordero');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,41,'Excelente acompa para jamón serrano, prosciutto o paté');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(12,41,'Ideal para vegetales como espárragos o espinaca');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(1,41,'Se consigue un buen maridaje al combinarle con sopa de pescado');	
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(4,27,'Ideal para platillos gratinados o cargados de queso');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(12,27,'Armoniza muy bien con vegetales rostizados');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(13,27,'Su maridaje con ensaladas es muy armonioso');		
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,43,'Acompañar con carnes grilladas o a la parrillas');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(15,43,'Ideal para el Confit de pato');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(1,24,'Su maridaje con pescados es ideal');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(2,24,'Buena opción para mariscos grillados o salteados');			
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(8,24,'Armoniza muy bien con pastas en salsas de frutos del mar');	
+
+
+

4cd6c18acce1675bdb730c28f5e3e4ef657c4fb4
gbsojo <gbsojo@gmail.com>
2015-05-27 22:14:53 -0400

    Inserts de especialistas

---
+++ sql/insert_especialista.sql
@@ -0,0 +1,44 @@
+/* 
+
+Especialista
+Interseccion catador_consultor
+*/
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,2);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,14);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,15);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,16);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,17);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,18);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,19);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,20);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,21);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,22);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (2,23);
+
+insert into especialista (fk_region,fk_catador_consultor)
+values (9,24);
+
+
+

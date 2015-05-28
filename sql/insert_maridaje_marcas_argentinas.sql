f21ab34635b2eae44326e6997fe100f65c83dfa3
gbsojo <gbsojo@gmail.com>
2015-05-28 00:53:49 -0400

    Inserts de los maridajes de vinos argentinos

---
+++ sql/insert_maridaje_marcas_argentinas.sql
@@ -0,0 +1,49 @@
+/*
+Maridajes vinos argentinos
+*/
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,2,'Buena opción para carnes blancas como el pollo');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,2,'Equilibrio de sabores adecuado para carnes a la parrilla');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(4,2,'Ideal para quesos suaves');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(8,2,'Ideal para pastas de salsas ligeras');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,3,'Armoniza perfectamente con la carne roja de pato');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,3,'Buena opción para carnes blancas como el pollo');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(8,3,'Excelente opción para pastas y platillos italianos');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,4,'Armoniza perfectamente con cualquier carne roja. Especialmente la de pato');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(3,4,'Buena opción para carnes blancas como el pollo');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(8,4,'Excelente opción para pastas y platillos italianos');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,5,'Armoniza perfectamente con la cane de cerdo, incluyendo las costillas');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(10,5,'Sus notas frutales son excelente opción para postres con chocolate');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(11,5,'Ideal para acompañar empanadas criollas');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(5,6,'Excelente opción para las carnes rojas grilladas o a la parrilla');
+
+insert into maridaje (fk_comida, fk_marca, mar_descripcion) values
+	(4,6,'Buena opción para acompañar quesos duros');
+

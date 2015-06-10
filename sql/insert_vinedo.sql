d345ffaef737f68787b90d9a6c86483508c86879
gbsojo <gbsojo@gmail.com>
2015-06-10 01:12:19 -0400

    Scripts vinedo, moneda, concurso

---
+++ sql/insert_vinedo.sql
@@ -0,0 +1,31 @@
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','LAGARDE Bodega','Finca Drummond');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','LAGARDE Bodega','Finca La Jacintana');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','LAGARDE Bodega','Finca La Vieja');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','LAGARDE Bodega','Finca Pircas');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','LAGARDE Bodega','Finca Las Bateas');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','LAGARDE Bodega','Finca Aguas Verdez');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','Bodega Nieto Senetiner','Vistalba');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','NORTON Bodega','La Colonia');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','NORTON Bodega','Perdriel');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','NORTON Bodega','Agrelo');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','NORTON Bodega','Lunlunta');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','NORTON Bodega','Medrano');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','Luigi Bosca','Los Nobles');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','Luigi Bosca','Los Miradores');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','Luigi Bosca','La Puntilla');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','Luigi Bosca','La Linda');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','Luigi Bosca','La España');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','Luigi Bosca','El Paraiso');
+select add_vinedo('Argentina','Mendoza','LUJÁN DE CUYO','MALBEC','Luigi Bosca','Don Leoncio');
+select add_vinedo('Italia','Sardegna','Cannonau di Sardegna','Cannonau Di Sardegna','Montespada','Surrau');
+select add_vinedo('Italia','Sardegna','Vermentino di Gallura','Vermentino Di Gallura','Montespada','Gostolai');
+select add_vinedo('Italia','Emilia Romagna','Colli Bolognesi','Barbera','Bonfiglio','Bazzano');
+select add_vinedo('Italia','Emilia Romagna','Colli Bolognesi','Barbera','Bonfiglio','Crespellano');
+select add_vinedo('Italia','Emilia Romagna','Colli Bolognesi','Barbera','Bonfiglio','Monteveglio');
+/*
+select add_vinedo('Italia','Emilia Romagna','Colli di Luni','Sangiovese','Cantine LVNAE','Ortonovo');
+select add_vinedo('Italia','Emilia Romagna','Colli di Luni','Sangiovese','Cantine LVNAE','Castelnuovo Magra');
+*/
+
+
+
